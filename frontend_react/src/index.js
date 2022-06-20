8419c5e9b845857f584040ef2fb86b56c25a00e1
GDIV <gdavisiv@gmail.com>
2022-06-20 15:55:23 -0400

    Ran: npm upate to update all out of date modules

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -4,4 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+//This will allow for the easy segmentation 
 ReactDOM.render(<App />, document.getElementById('root'));

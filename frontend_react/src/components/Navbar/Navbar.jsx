cc6a58e1af8e4c6acc38c8f31b4d4e2bea015d8c
GDIV <gdavisiv@gmail.com>
2022-07-12 23:41:14 -0400

    NavBar: Creating SCSS/Animations

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { images } from '../../constants';
+import './Navbar.scss';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {/* This allows us to call all the items below from an array */}
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+
   )
 }
 

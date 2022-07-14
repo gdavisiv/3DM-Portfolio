a4615a515da9e72f6df797575537983169fe9502
GDIV <gdavisiv@gmail.com>
2022-07-14 08:11:56 -0400

    NavBar: Implement Mobile View MenuBar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [toggle, settoggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -18,9 +22,29 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      
+      <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+          {/* IF toggle is true we want to render our mobile navigation Bar */}
+          {
+            toggle && (
+              //Used from framer.com/developers
+              <motion.div
+                whileInView={{ x: [300, 0] }}
+                transition={{ duration: 0.85, ease: 'easeOut' }}
+              >
+              <HiX onClick={() => setToggle(false)} />
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={{item}}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                </li>
+              ))}
+              </motion.div>
+            )}
+      </div>
     </nav>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;

2c38ee540e47426f74f38cbe93186c05a0f10fa0
GDIV <gdavisiv@gmail.com>
2022-07-15 21:26:42 -0400

    Header: Building out Header jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 import './Header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0,1] }}
+        transition={{ duration: 1.25 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, and welcome to</p>
+              <h1 className="head-text">3DMations</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp">
+
+          </div>
+        </div>
+      </motion.div>
+    </div>
   );
-}
+};
 
 export default Header;

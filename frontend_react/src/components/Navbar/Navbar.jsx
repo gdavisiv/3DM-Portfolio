4cb5ad9d6d8f0e0f158d0a2b41c0647f7a69d44d
GDIV <gdavisiv@gmail.com>
2022-07-17 19:39:14 -0400

    Navbar Update FINAL

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {/* This allows us to call all the items below from an array */}
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
@@ -22,29 +21,27 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      
-      <div className="app__navbar-menu">
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
 
-          {/* IF toggle is true we want to render our mobile navigation Bar */}
-          {
-            toggle && (
-              //Used from framer.com/developers
-              <motion.div
-                whileInView={{ x: [300, 0] }}
-                transition={{ duration: 0.85, ease: 'easeOut' }}
-              >
-              <HiX onClick={() => setToggle(false)} />
-              <ul>
-                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                  <li key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-              </motion.div>
-            )}
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
       </div>
     </nav>
   );

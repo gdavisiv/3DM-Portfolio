45b1b77142f90b3979bb71463af5df7e8a997488
GDIV <gdavisiv@gmail.com>
2022-07-18 08:00:40 -0400

    Build About Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 import './About.scss';
 
+const abouts = [
+  { title: 'Web Development', description: 'I build website with my full stack experience', imgUrl: '' }
+];
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className="head-text">Simplicity
+      <span>is about subtracting the obvious</span>
+      <br />
+      and
+      <span>adding the meaningful. - John Maeda</span>
+    </h2>
+
+      <div className="app__profiles">
+
+      </div>
+
+    </>
   );
-}
+};
 
 export default About;

7ac111d4f24edfce60738bc931cc08482f3c1a92
GDIV <gdavisiv@gmail.com>
2022-07-20 22:09:37 -0400

    About: Updated JSX classname

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -22,8 +22,16 @@ const About = () => {
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
-          <motion.div>
-            
+          <motion.div
+            whileInView={{ opacity: 1}}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+              <h2 className="bold-text" style={{ marginTop: 20}}>{about.title}</h2>
+
           </motion.div>
         ))}
       </div>

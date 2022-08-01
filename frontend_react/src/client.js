3e62501bae3d0adedc85609a4ad079335c4394cc
GDIV <gdavisiv@gmail.com>
2022-07-31 21:47:40 -0400

    Updated CRM info. Added to .gitignore

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -3,8 +3,8 @@ import imgURLBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
     projectId: 'x3nehxf1',
-    dataset: '',
-    apiVersion: '',
-    useCdn: '',
-    token: '',
+    dataset: 'production',
+    apiVersion: '2022-02-01',
+    useCdn: true,
+    token: 'sk9Bqg6C2TCxnxmsA13QXUoiuMtrRQnEfxyGG9XbYzqjRxzfTSncCUMqaHvqUM24wbmngDzMyDc0zOZT6G0p5h5EapE2h9Q1kwzQPXTBC2SdzCkpSAz5FMM6JxpvClSU1nh9dmhp8tLDDgAy60jctCgkvATngrvvXud1g7aaQPe3dpKwizIt',
 });

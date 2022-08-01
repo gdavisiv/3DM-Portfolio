62a042532fb2d3c52248e31f864ef72e6b9deb3c
GDIV <gdavisiv@gmail.com>
2022-07-31 22:04:08 -0400

    Updated CMS: API

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,9 +2,13 @@ import sanityClient from '@sanity/client';
 import imgURLBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'x3nehxf1',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'sk9Bqg6C2TCxnxmsA13QXUoiuMtrRQnEfxyGG9XbYzqjRxzfTSncCUMqaHvqUM24wbmngDzMyDc0zOZT6G0p5h5EapE2h9Q1kwzQPXTBC2SdzCkpSAz5FMM6JxpvClSU1nh9dmhp8tLDDgAy60jctCgkvATngrvvXud1g7aaQPe3dpKwizIt',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageURLBuilder(client);
+
+export const urlFor = (source) => builder.image(source);

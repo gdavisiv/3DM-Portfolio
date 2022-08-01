0bb7712bdd46daa2ba79a46f3155aa97de3f6663
GDIV <gdavisiv@gmail.com>
2022-08-01 08:43:00 -0400

    Updates for connection with Sanity CMS

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,19 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-  { title: 'Full Stack Web Dev', description: 'I build website with my full stack experience', imgUrl: images.about01 },
-  { title: 'iOS Dev', description: 'I build website with my full stack experience', imgUrl: images.about02 },
-  { title: 'OpenCV/AI/ML', description: 'I build website with my full stack experience', imgUrl: images.about03 },
-  { title: 'Web 3.0', description: 'I build website with my full stack experience', imgUrl: images.about04 }
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
     <h2 className="head-text">Simplicity
@@ -30,7 +34,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
                 < p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>

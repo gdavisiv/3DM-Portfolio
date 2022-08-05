45c2f01bf4349ecd457e1eb86857bdf7c54df0ae
GDIV <gdavisiv@gmail.com>
2022-08-05 18:43:03 -0400

    Update AppWrap.js

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { NavigatonDots, SocialMedia } from '../components'
 
-const AppWrap = () => {
+//AppWrap will take in the following Compoent, idName, and className, and will return
+//the function HOC
+const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
-    <div>AppWrap</div>
+    <div id={idName} className={'app__container ${classNames}'}>
+      <SocialMedia />
+
+      <div className="app__wrapper app__flex">
+        <Component />
+      </div>
+    </div>
   )
 }
 

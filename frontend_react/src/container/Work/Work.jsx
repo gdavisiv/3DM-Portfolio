94900be5df7c1039d79a2f50d37c13aaab9567fd
GDIV <gdavisiv@gmail.com>
2022-08-08 08:33:54 -0400

    Creating Title Bar for Work Section along with needed import{}

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -3,13 +3,23 @@ import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
 import { AppWrap } from '../../wrapper';
-
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">Examples of
+      <span> projects completed</span>
+      <br />
+      for
+      <span> various clientelle.</span>
+    </h2>
+    <div className="app__work-filter">
+      {['UI/UX', 'Web App', 'Mobile App', 'CyberSecurity']}
+    </div>
+    </>
   );
-}
+};
 
 export default Work;

9d16b56385f8cf9a69c94ac4693123461044fd45
GDIV <gdavisiv@gmail.com>
2022-08-17 08:02:06 -0400

    Updating Services Section

--- frontend_react/src/container/Services/Services.jsx
+++ frontend_react/src/container/Services/Services.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -46,7 +47,31 @@ const Services = () => {
             </motion.div>
           ))}
         </motion.div>
-        
+        <motion.div className="app__services-exp">
+          {experiences.works.map((work) => (
+            <>
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__services-exp-work"
+              data-tip
+              data-for={work.name}
+              key={work.name}
+            >
+              <h4 className="bold-text">{work.name}</h4>
+              <p className="p-text">{work.company}</p>             
+            </motion.div>
+            <ReactTooltip
+              id={work.name}
+              effect="solid"
+              arrowColor="#fff"
+              className="skills-tooltip"
+            >
+              {work.desc}
+            </ReactTooltip>
+            </>
+          ))}
+        </motion.div>
       </div>
     </>
   );

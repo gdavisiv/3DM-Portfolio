974dea6d09b3900bd0f69808f60ee40c2ee01a0b
GDIV <gdavisiv@gmail.com>
2022-08-20 07:58:56 -0400

    Building Testimonials Section

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {

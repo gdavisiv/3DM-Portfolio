f88c04232fe77509fee6ecd4dee4ae061257a21e
GDIV <gdavisiv@gmail.com>
2022-08-22 21:50:20 -0400

    Updated Footer with Accurate Interactive FORM

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
@@ -6,61 +6,75 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-  const [formData, setFormData] = useState({ name:'', email: '', message: '' });
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { name, email, message } = formData;
-  
-  const handleChangeInput = (e) => {
-    const  { name, value } = e.target;
+  const { username, email, message } = formData;
 
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-  }
+  };
 
   const handleSubmit = () => {
     setLoading(true);
 
     const contact = {
       _type: 'contact',
-      name: name,
-      email: email,
-      message: message
-    }
-  }
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-    <h2 className="head-text"><span>Contact us</span> so we can turn your <span>vision</span> into <span>tangible results.</span></h2>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
 
-    <div className="app__footer-cards">
-      <div className="app__footer-card">
-        <img src={images.email} alt="email" />
-        <a href="mailto:contact@3dmations.com" className="p-text">Contact@3DMations.com</a>
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a href="mailto:contact@3dmations.com" className="p-text">Contact@3DMations.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+1 (123) 456-7890" className="p-text">tel: +1 (347) 559-7477</a>
+        </div>
       </div>
-      <div className="app__footer-card">
-        <img src={images.mobile} alt="mobile" />
-        <a href="tel: +1 (347) 559-7477" className="p-text">+1 (347) 559-7477</a>
-      </div>
+      {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="name" value={name} onChange={handleChangeInput}/>
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
           </div>
           <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput}/>
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
           </div>
-        </div>
           <div>
             <textarea
               className="p-text"
               placeholder="Your Message"
               value={message}
-              name={message}
+              name="message"
               onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'}</button>
-    </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for getting in touch!
+          </h3>
+        </div>
+      )}
     </>
   );
 };
@@ -68,5 +82,5 @@ const Footer = () => {
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),
   'contact',
-  'app_whitebg'
+  'app__whitebg',
 );

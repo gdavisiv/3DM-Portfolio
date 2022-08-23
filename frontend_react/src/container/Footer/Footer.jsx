1e07a2ec6c57d418a282e58365454a475c630dfb
GDIV <gdavisiv@gmail.com>
2022-08-22 21:11:34 -0400

    Update Footer.jsx

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -6,10 +6,64 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
+  const [formData, setFormData] = useState({ name:'', email: '', message: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+  
+  const handleChangeInput = (e) => {
+    const  { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: name,
+      email: email,
+      message: message
+    }
+  }
+
   return (
-    <div>Footer</div>
+    <>
+    <h2 className="head-text"><span>Contact us</span> so we can turn your <span>vision</span> into <span>tangible results.</span></h2>
+
+    <div className="app__footer-cards">
+      <div className="app__footer-card">
+        <img src={images.email} alt="email" />
+        <a href="mailto:contact@3dmations.com" className="p-text">Contact@3DMations.com</a>
+      </div>
+      <div className="app__footer-card">
+        <img src={images.mobile} alt="mobile" />
+        <a href="tel: +1 (347) 559-7477" className="p-text">+1 (347) 559-7477</a>
+      </div>
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="name" value={name} onChange={handleChangeInput}/>
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput}/>
+          </div>
+        </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name={message}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'}</button>
+    </div>
+    </>
   );
-}
+};
 
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),

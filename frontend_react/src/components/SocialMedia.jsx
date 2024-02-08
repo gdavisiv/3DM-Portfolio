72359a57ade7ca0d2a2692af54bca716e92871f4
GDIV <gdavisiv@gmail.com>
2024-02-08 09:56:35 -0500

    Edit: Updated Link/Improved Aesthetics

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,12 +1,16 @@
+//Library of all Icons that might be needed:
+//      https://react-icons.github.io/react-icons/search/#q=upwork
+//Rember to use the specific library.
 import React from 'react';
-import { BsTwitter, BsLinkedin, BsGithub } from 'react-icons/bs';
-//import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { SiUpwork } from "react-icons/si";
+
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <a href="https://twitter.com/gdavisiv"><BsTwitter /></a>
+            <a href="https://www.upwork.com/agencies/1613007447754076160/"><SiUpwork /></a>
         </div>
         <div>
         <a href="https://www.linkedin.com/in/gdavisiv/"><BsLinkedin /></a>
